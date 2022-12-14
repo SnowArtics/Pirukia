73cb0ffad6b1161c913ba59222fd6566426aec97
dhks103 <dhks103@naver.com>
2022-12-15 03:34:11 +0900

    feat(database): Add SQLite connecting
    Make BuildStructure.cs
    Make BelieverIdle.cs
    Make Prayer sprite sample

---
+++ Assets/Scripts/BelieverIdle.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelieverIdle : MonoBehaviour
+{
+    [SerializeField]
+    private new string name;        // 신도의 이름
+    [SerializeField]
+    private int speed;              // 신도의 이동 속도
+    [SerializeField]
+    private int loyalty;            // 신도의 충성도
+    [SerializeField]
+    private new string animation;   // 출력할 애니메이션
+    [SerializeField]
+    private int group;              // 해당 신도가 믿는 신의 그룹번호
+
+    private AnimationWalkList nowWalk;
+    private float moveTime = 0;
+    private enum AnimationWalkList  { none,
+        Char_Walk_SouthWest, Char_Walk_South, Char_Walk_EastSouth,
+        Char_Walk_West, Char_IDLE, Char_Walk_East,
+        Char_Walk_WestNorth, Char_Walk_North, Char_Walk_NorthEast
+    };
+    private enum AnimationPrayList { none,
+        Char_Pray_East, Char_Pray_West
+    };
+
+    public void SetAnimation(string animation) {
+        this.animation = animation;
+    }
+
+    string GetAnimation(int animation) {
+        string walkMotion = "Anim_" + name + (AnimationWalkList) animation;
+        return walkMotion;
+    }
+
+    void Walk(int direction) {
+        Vector3 pos = transform.position;
+
+        switch (direction) {
+            case 2: {       // 남쪽으로 이동
+                    transform.Translate((new Vector3(0,0,-1)) * speed * Time.deltaTime);
+                    break;
+            }
+            case 4: {       // 서쪽으로 이동
+                    transform.Translate((new Vector3(-1,0,0)) * speed * Time.deltaTime);
+                    break;
+            }
+            case 6: {       // 동쪽으로 이동
+                    transform.Translate((new Vector3(1,0,0)) * speed * Time.deltaTime);
+                    break;
+            }
+            case 8: {       // 북쪽으로 이동
+                    transform.Translate((new Vector3(0,0,1)) * speed * Time.deltaTime);
+                    break;
+            }
+        }
+    }
+
+    public void RandomWalk(int direction) {
+        this.animation = GetAnimation(group + direction);       // 신 분류 코드(2자리) + 행동 코드(2자리)
+        SetAnimation(animation);
+
+        Walk(direction);
+    }
+
+    public void SetSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void SetLoyalty(int loyalty) {
+        this.loyalty = loyalty;
+    }
+
+    public void Awake() {
+        SetSpeed(3);
+        SetLoyalty(10);
+    }
+
+    public void Update() {
+        moveTime += Time.deltaTime;
+
+        // 5초가 지나면 RandomWalk() 함수를 실행하는 Idle() 코루틴 실행
+        if (moveTime >= 5) {
+            StartCoroutine(Idle());
+        }
+    }
+
+    public IEnumerator Idle() {
+        int direction = (Random.Range(1, 4) * 2);                    // 2,4,6,8 내에서 난수 생성하여 임의의 방향 지정
+        moveTime = 0;
+
+        // 2초 동안 RandomWalk() 함수 실행
+        for(moveTime = 0; moveTime <= 2; moveTime += Time.deltaTime) { 
+            RandomWalk(direction);
+            yield return null;
+        }
+
+        // 2초 경과 후, 코루틴 종료(다시 대기 상태로 전환)
+        moveTime = 0;
+        yield break;
+    }
+}
---
+++ Assets/Scripts/BuildStructure.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class BuildStructure : MonoBehaviour
+{
+
+    void Start()
+    {
+        DBCreate();
+        DBConnectionCheck();
+        DatabaseRead("SELECT * from build");
+    }
+
+    void Awake() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DBCreate() {
+        string filepath = string.Empty;                                                     // 파일 경로
+
+        filepath = Application.dataPath + "/StreamingAssets/Building_Database.db";          // 경로 설정
+        if (!File.Exists(filepath)) {                                                       // 파일이 없을 때
+            File.Copy(Application.streamingAssetsPath + "/StreamingAssets/Building_Database.db", filepath);      // 해당 경로로 파일 복사
+        }
+
+        Debug.Log("DB 생성 완료");
+    }
+
+    public string GetDBFilePath() {
+        string str = string.Empty;
+
+        str = "URI=file:" + Application.dataPath + "/StreamingAssets/Building_Database.db";
+
+        return str;
+    }
+
+    public void DBConnectionCheck() {
+        IDbConnection dbConnection = new SqliteConnection(GetDBFilePath());
+        dbConnection.Open();
+
+        if(dbConnection.State == ConnectionState.Open) {
+            Debug.Log("DB 연결 성공");
+        }
+        else {
+            Debug.Log("연결 실패(에러)");
+        }
+    }
+
+    public void DatabaseRead(string query) {        // 인자로 쿼리문을 받는다.
+        // DB 열기
+        IDbConnection dbConn = new SqliteConnection(GetDBFilePath());
+        dbConn.Open();
+        
+        // 쿼리 입력
+        IDbCommand dbCommand = dbConn.CreateCommand();
+        dbCommand.CommandText = query;
+
+        // 쿼리 실행
+        IDataReader dataReader = dbCommand.ExecuteReader();
+
+        while (dataReader.Read()) {
+            // 0번, 1번, 2번 필드 읽기
+            Debug.Log(dataReader.GetInt32(0) + ", " + dataReader.GetString(1) + ", " + dataReader.GetString(2));
+        }
+
+        // 생성 순서와 반대로 닫아준다.
+        dataReader.Dispose();
+        dataReader = null;
+        dbCommand.Dispose();
+        dbCommand = null;
+        dbConn.Close();
+        dbConn = null;
+    }
+}
