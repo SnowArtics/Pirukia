a9f319a18d3df7efd67be5e8d199fe244fc9db61
dhks103 <dhks103@naver.com>
2023-01-28 16:14:21 +0900

    Revert "Restore files"
    This reverts commit cb44763e390c8ce552eeb16384af2817ad3c2d8b.

--- Assets/Scripts/BuildUIScripts/BuildUITooltip.cs
+++ Assets/Scripts/BuildUIScripts/BuildUITooltip.cs
@@ -1,26 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BuildUITooltip : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject tooltipUI;
-    [SerializeField]
-    private TextMeshProUGUI infoBuild;
-
-    public void ShowTooltip(string info) {
-        tooltipUI.SetActive(true);
-        infoBuild.text = info;
-
-        for(; ; ) {
-
-        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
-    public void HideTooltip(string info) {
-        tooltipUI.SetActive(false);
+    public void OnMouseOver() {
+        Debug.Log("Tooltip!!!!!!");
     }
 }
