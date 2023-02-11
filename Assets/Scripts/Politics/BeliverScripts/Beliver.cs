394aa4f2045833c102eba2d6ba876ee923fa9e6b
kdm1362 <kdm1362@gmail.com>
2023-02-12 02:02:03 +0900

    Project structure refactoring and terminology unification
    프로젝트 구조 리팩터링 및 용어 통일
    *NPC->Beliver
    *create sub directory in Scripts

--- Assets/Scripts/Politics/BeliverScripts/Beliver.cs
+++ Assets/Scripts/Politics/BeliverScripts/Beliver.cs
@@ -2,37 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+public class Beliver : MonoBehaviour
 {
-    // npc의 고유한 이름
+    // beliver의 고유한 이름
     [SerializeField]
-    private string npcName;
+    private string beliverName;
 
-    // npc의 이동속도
+    // beliver의 이동속도
     [SerializeField]
     private int speed;
 
-    // npc가 수행해야하는 행동
+    // beliver가 수행해야하는 행동
     [SerializeField]
-    private string npcAnimation;
+    private string beliverAnimation;
     
-    // npc가 속한 세력
+    // beliver가 속한 세력
     [SerializeField]
     private int group;
     
-    // npc가 지닌 충성도
+    // beliver가 지닌 충성도
     [SerializeField]
     private int loyalty;
     
-    // npc가 속한 작업장
+    // beliver가 속한 작업장
     [SerializeField]
     private int workGroup;
     
-    // npc의 작업속도
+    // beliver의 작업속도
     [SerializeField]
     private float workSpeed;
 
-    // npc의 이동속도 조정 메서드
+    // beliver의 이동속도 조정 메서드
     public void SetSpeed(int speed)
     {
         // 입력된 속도가 정상적인지 확인 필요
@@ -43,7 +43,7 @@ public class NPC : MonoBehaviour
         return this.speed;
     }
 
-    // npc의 충성도 조정 메서드
+    // beliver의 충성도 조정 메서드
     public void SetLoyalty(int loyalty)
     {
         this.loyalty = loyalty;
@@ -61,7 +61,7 @@ public class NPC : MonoBehaviour
         return this.loyalty;
     }
 
-    // npc의 작업속도 조정 메서드
+    // beliver의 작업속도 조정 메서드
     public void SetWorkSpeed(int workspeed)
         // TODO: 매개변수와 속성의 타임을 일치시킬 필요가 있음
     {
@@ -83,10 +83,10 @@ public class NPC : MonoBehaviour
     // 애니메이션 설정
     public void SetAnimation(string animation)
     {
-        this.npcAnimation = animation;
+        this.beliverAnimation = animation;
     }
 
-    // npc의 소속 작업장 조정 메서드
+    // beliver의 소속 작업장 조정 메서드
     public void SetWorkGroup(int workgroup)
     {
         this.workGroup = workgroup;
@@ -96,7 +96,7 @@ public class NPC : MonoBehaviour
         return this.workGroup;
     }
 
-    // npc의 위치이동
+    // beliver의 위치이동
     public void RandomWalk()
     {
         // TODO: 게임오브젝트를 무작위로 이동시켜야 함.
@@ -110,7 +110,7 @@ public class NPC : MonoBehaviour
 
     // animation DB접근
     // string GetAnimation(int serial)
-    // TODO: 가능하면 NPC관리 클래스에서 처리하고 싶음
+    // TODO: 가능하면 Beliver관리 클래스에서 처리하고 싶음
     public string QueryAnimation(int serial)
     {
         // TODO: DB접근 구현
